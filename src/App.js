70b2b4b5c26fb126ef92ab390c2f2fe5384a55d0
Javi Sastre <javisastrecal@gmail.com>
2021-08-31 13:13:32 +0200

    basic styling setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import logo from "./logo.svg";
-import "./App.css";
+const API = {
+  key: "0c85f5856ac69992b41e85a1abd59f05",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <main>
+        <div className='search-box'>
+          <input type='text' className='search-bar' placeholder='Search...' />
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
